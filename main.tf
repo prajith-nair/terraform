c01e1a029c5e01a44e2387f6bbcd6db61d62972c
Prajith Nair <prajithnair87@gmail.com>
2021-04-19 21:41:43 +0530

    Changed tag name

--- main.tf
+++ main.tf
@@ -8,9 +8,25 @@ provider "aws" {
 }
 
 resource "aws_instance" "lab" {
-ami = "ami-0c55b159cbfafe1f0"
-instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  user_data     = <<-EOF
+    #!/bin/bash
+    echo "Terraform world" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
   tags = {
     Name = "terraform-lab"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name  = "terraform-sg"
+  ingress
+  {
+   from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
