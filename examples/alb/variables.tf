723a2cf151a8bfe23166c6678c194bf641df0202
Prajith Nair <prajithnair87@gmail.com>
2021-05-19 15:34:56 +0530

    change default to custom name

--- examples/alb/variables.tf
+++ examples/alb/variables.tf
@@ -6,5 +6,5 @@
 variable "alb_name" {
   description = "The name of the ALB and all its resources"
   type        = string
-  default     = "terraform-up-and-running"
+  default     = "prajith-terraform-lab"
 }
