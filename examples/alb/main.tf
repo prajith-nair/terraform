a6105348aada7c5ecd23f0e470ae4bbaff25dd16
Prajith Nair <prajith@happygears.net>
2021-05-19 15:31:16 +0530

    Created submodules and sample hello-world-app module

---
+++ examples/alb/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "prajithnairsolutions"
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+module "alb" {
+  source = "../../modules/networking/alb"
+
+  alb_name   = var.alb_name
+  subnet_ids = data.aws_subnet_ids.default.ids
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
---
+++ examples/mysql/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-2"
+  profile = "prajithnairsolutions"
+}
+
+module "mysql" {
+  source = "../../modules/data-stores/mysql"
+
+  db_name     = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
+}
